04ddd5995d33e9277bba80fcd39f67de07af0a0d
Annisa_Asyrof <annisa.asyrof04@gmail.com>
2023-10-06 07:25:03 +0700

    Prepare deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-/* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -19,8 +20,9 @@ function App() {
       </Routes>
     </div>
     </Router>
+    <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
